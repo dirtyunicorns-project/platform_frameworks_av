cdd7587ec603247c78c8149976a18809c2bca8ba
DennySPB <dennyspb@gmail.com>
2018-05-08 09:02:43 +0300

    Revert "media: fix blurred image bug for thumbnail of video"
    This reverts commit 09cdcc55873a3e303994ff138cd84ea96fbfd00c.

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -502,11 +502,10 @@ static VideoFrame *extractVideoFrame(
                     ALOGV("Received an output buffer, timeUs=%lld", (long long)timeUs);
                     sp<MediaCodecBuffer> videoFrameBuffer = outputBuffers.itemAt(index);
 
-                    int32_t width, height, stride;
+                    int32_t width, height;
                     CHECK(outputFormat != NULL);
                     CHECK(outputFormat->findInt32("width", &width));
                     CHECK(outputFormat->findInt32("height", &height));
-                    CHECK(outputFormat->findInt32("stride", &stride));
 
                     int32_t crop_left, crop_top, crop_right, crop_bottom;
                     if (!outputFormat->findRect("crop", &crop_left, &crop_top, &crop_right, &crop_bottom)) {
@@ -545,7 +544,7 @@ static VideoFrame *extractVideoFrame(
                     if (converter.isValid()) {
                         err = converter.convert(
                                 (const uint8_t *)videoFrameBuffer->data(),
-                                stride, height,
+                                width, height,
                                 crop_left, crop_top, crop_right, crop_bottom,
                                 frame->mData,
                                 frame->mWidth,
